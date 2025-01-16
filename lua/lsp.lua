d0e9ca264a7bebd999d9007164dfc2e66d809360
Johan Montorfano <me@johanmontorfano.com>
2025-01-16 03:21:28 +0100

    rm: preset + replace: tsserver by ts_ls

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,7 +2,7 @@ local lsp_conf = require("lspconfig")
 
 lsp_conf.lua_ls.setup {}
 lsp_conf.pyright.setup {}
-lsp_conf.tsserver.setup {}
+lsp_conf.ts_ls.setup {}
 lsp_conf.rust_analyzer.setup {}
 lsp_conf.somesass_ls.setup {}
 lsp_conf.clangd.setup {}
--- lua/lspzeroc.lua
+++ lua/lspzeroc.lua
@@ -1,8 +1,6 @@
 local lsp = require("lsp-zero")
 local cmp = require("cmp")
 
-lsp.preset("recommended")
-
 cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ["<CR>"] = cmp.mapping.confirm({select = true})
