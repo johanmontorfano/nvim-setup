dae0adcacfb3fd329212dd683ff0d61084bc7624
Johan Montorfano <me@johanmontorfano.com>
2025-01-17 00:53:57 +0100

    fix: lsp setup

--- lua/lspzeroc.lua
+++ lua/lspzeroc.lua
@@ -2,6 +2,9 @@ local lsp = require("lsp-zero")
 local cmp = require("cmp")
 
 cmp.setup({
+    sources = { 
+        { name = "nvim_lsp" }
+    },
     mapping = cmp.mapping.preset.insert({
         ["<CR>"] = cmp.mapping.confirm({select = true})
     }),
