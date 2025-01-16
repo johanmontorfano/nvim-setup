f0ff324afc88823587ccdeb2fa91ee070f66f790
Johan Montorfano <me@johanmontorfano.com>
2025-01-16 02:31:27 +0100

    fix: mappings have been formatted for wk v3

--- lua/plugs-maps.lua
+++ lua/plugs-maps.lua
@@ -1,55 +1,33 @@
 local wk = require("which-key")
 
-wk.register({
-    -- which key bindings
-    wk = { "<cmd>:WhichKey<CR>", "Which Key" },
-    -- telescope bindings
-	f = {
-		name = "Telescope",
-		f = { "<cmd>:Telescope find_files<CR>", "Find files" },
-		g = { "<cmd>:Telescope live_grep<CR>", "Live grep"},
-		b = { "<cmd>:Telescope buffers<CR>", "Find buffers" },
-		h = { "<cmd>:Telescope help_tags<CR>", "Help tags" }
-	},
-    -- session bindings
-    s = {
-        name = "Session",
-        s = { "<cmd>:SSave<CR>", "Save session" },
-        c = { "<cmd>:SClose<CR>", "Close session" }
-    },
-    -- mason bindings
-    m = {
-        name = "Mason",
-        o = { "<cmd>:Mason<CR>", "Mason" },
-        u = { "<cmd>:MasonUpdate<CR>", "Update Mason packages" }
-    },
-    -- lsp-zero bindings
-    l = {
-        name = "LSP",
-        a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-        i = { "<cmd>LspInfo<cr>", "Info" },
-        l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-        r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-        s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
-        S = {
-            "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
-            "Workspace Symbols",
-        }
-    },
-    -- nvim-tree bindings
-    n = {
-        name = "nvim-tree",
-        o = { "<cmd>:NvimTreeOpen<CR>", "Open Nvim Tree" },
-        t = { "<cmd>:NvimTreeToggle<CR>", "Toggle Nvim Tree" },
-        c = { "<cmd>:NvimTreeClose<CR>", "Close Nvim Tree" },
-        f = { "<cmd>:NvimTreeFocus<CR>", "Focus Nvim Tree" }
-    },
-    -- trouble bindings
-    t = {
-        name = "Trouble",
-        t = {"<cmd>:TroubleToggle<CR>", "Toggle"},
-        q = {"<cmd>:TroubleToggle quickfix<CR>", "Quickfix"}
-    },
-    -- todo-comments bindings
-    cq = {"<cmd>:TodoQuickFix<CR>", "Todo QuickFix"},
-}, { prefix = "p" })
+wk.add({
+    { "pcq", "<cmd>:TodoQuickFix<CR>", desc = "Todo QuickFix" },
+    { "pf", group = "Telescope" },
+    { "pfb", "<cmd>:Telescope buffers<CR>", desc = "Find buffers" },
+    { "pff", "<cmd>:Telescope find_files<CR>", desc = "Find files" },
+    { "pfg", "<cmd>:Telescope live_grep<CR>", desc = "Live grep" },
+    { "pfh", "<cmd>:Telescope help_tags<CR>", desc = "Help tags" },
+    { "pl", group = "LSP" },
+    { "plS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", desc = "Workspace Symbols" },
+    { "pla", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "Code Action" },
+    { "pli", "<cmd>LspInfo<cr>", desc = "Info" },
+    { "pll", "<cmd>lua vim.lsp.codelens.run()<cr>", desc = "CodeLens Action" },
+    { "plr", "<cmd>lua vim.lsp.buf.rename()<cr>", desc = "Rename" },
+    { "pls", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document Symbols" },
+    { "pm", group = "Mason" },
+    { "pmo", "<cmd>:Mason<CR>", desc = "Mason" },
+    { "pmu", "<cmd>:MasonUpdate<CR>", desc = "Update Mason packages" },
+    { "pn", group = "nvim-tree" },
+    { "pnc", "<cmd>:NvimTreeClose<CR>", desc = "Close Nvim Tree" },
+    { "pnf", "<cmd>:NvimTreeFocus<CR>", desc = "Focus Nvim Tree" },
+    { "pno", "<cmd>:NvimTreeOpen<CR>", desc = "Open Nvim Tree" },
+    { "pnt", "<cmd>:NvimTreeToggle<CR>", desc = "Toggle Nvim Tree" },
+    { "ps", group = "Session" },
+    { "psc", "<cmd>:SClose<CR>", desc = "Close session" },
+    { "pss", "<cmd>:SSave<CR>", desc = "Save session" },
+    { "pt", group = "Trouble" },
+    { "ptq", "<cmd>:TroubleToggle quickfix<CR>", desc = "Quickfix" },
+    { "ptt", "<cmd>:TroubleToggle<CR>", desc = "Toggle" },
+    { "pwk", "<cmd>:WhichKey<CR>", desc = "Which Key" },
+  })
+
