b84fd5bd4f3a7b2e3e707ada2bfa0faad718c917
Johan Montorfano <me@johanmontorfano.com>
2025-02-10 00:09:56 +0100

    change: clangd to ccls

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -5,7 +5,13 @@ lsp_conf.pyright.setup {}
 lsp_conf.ts_ls.setup {}
 lsp_conf.rust_analyzer.setup {}
 lsp_conf.somesass_ls.setup {}
-lsp_conf.clangd.setup {}
+lsp_conf.ccls.setup {
+  init_options = {
+    compilationDatabaseDirectory = "build",
+    cache = { directory = ".ccls-cache" },
+    clang = { extraArgs = { "-I./include" } }
+  }
+}
 lsp_conf.html.setup {}
 lsp_conf.cssls.setup {}
 lsp_conf.bashls.setup {}
